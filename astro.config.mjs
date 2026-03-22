c2c005e0adf3b3f43380942f1f8266f8a4249902
PraveenDileesha <dileeshapraveen@gmail.com>
2026-03-23 02:18:33 +0530

    chore: remove GitHub Actions, clean astro config for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,2 @@
-// // @ts-check
-// import { defineConfig } from 'astro/config';
-
-// // https://astro.build/config
-// export default defineConfig({});
 import { defineConfig } from "astro/config";
-
-export default defineConfig({
-  site: "https://praveendileesha.github.io",
-  base: "/portfolio/",
-});
+export default defineConfig({});
